638da5fc2b0cae85697fb7e458a3c0741b53f19b
KwekuD <dansogen.rd@gmail.com>
2022-06-17 06:15:36 +0000

    take

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-* leet - a leet function
+* leet - a leet function 
 * @x: param x
 * Return: a string
 */
@@ -9,7 +9,7 @@ char *leet(char *x)
 {
 	int a = 0, b, l = 5;
 	char tr[5] = {'A', 'E', 'O', 'T', 'L'};
-	char trw[5] = ('4', '3', '0', '7', '1'};
+	char trw{5} = {'4', '3', '0', '7', '1'}
 
 	while (x[a])
 	{
@@ -21,7 +21,7 @@ char *leet(char *x)
 				x[a] = trw[b];
 			b++;
 		}
-		a++;
+		a++
 	}
 	return (x);
 }
