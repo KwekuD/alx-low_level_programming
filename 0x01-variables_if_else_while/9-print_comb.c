ba4ea67697a2c3606afe82d8fea117ed495b1446
KwekuD <dansogen.rd@gmail.com>
2022-06-17 00:49:30 +0000

    task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -14,12 +14,12 @@ for (ch = 48; ch <= 57; ch++)
 putchar(ch);
 if (ch != 57)
 {
-putchar(44)
-putchar(32)
+putchar(44);
+putchar(32);
 }
 }
 
-putchar(10) /* this is an ascii code for new line*/
+putchar(10); /* this is an ascii code for new line*/
 
 return (0);
 }
