45ac981d834b99d03702bccd4d9385b536ae6d98
KwekuD <dansogen.rd@gmail.com>
2022-06-17 03:21:23 +0000

    task 9 and 10

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,13 +1,33 @@
 #include "main.h"
 
 /**
- * add - adds two numbers
- * @x : param1
- * @y : param2
- * Return: x + y
+ * times_table - time table
  */
-
-int add(int x, int y)
+void times_table(void)
 {
-	return (x + y);
+	int x, y, k;
+
+	for (x = 0; x < 10; x++)
+	{
+		for (y = 0; y < 10; y++)
+		{
+			k = x * y;
+			if (y == 0)
+				_putchar(k + '0');
+			if  (y != 0 && k < 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(k + '0');
+			} else if (k >= 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar((k / 10) + '0');
+				_putchar((k % 10) + '0');
+			}
+		}
+		_putchar('\n')
+	}
 }
